27aa94f1da95d76689e85474e88b59fe02ba8f12
Jacob0418 <cjosecoronado97@gmail.com>
2024-01-25 09:22:25 -0500

    estilos

--- tallerUT/src/App.jsx
+++ tallerUT/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <Navbar/>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +18,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-black'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-black'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-black">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ tallerUT/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <>
+            <header className=" m-0 p-0 top-0 bg-red-600 fixed w-full z-50">
+                <div className=" flex p-10">
+                    <nav className=" flex items-center basis-3/5 m-0 p-0 top-0">
+                        TALLER UT
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Navbar;
