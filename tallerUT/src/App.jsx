f6ec421d9910acf65f159513351e1c216cb9c775
soto1231XD <alvarez.01.jlsa@gmail.com>
2024-01-28 01:22:40 -0500

    inicio y registro user

--- tallerUT/src/App.jsx
+++ tallerUT/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/header'
 import Inicio from './pages/Inicio'
 import NotFound from './pages/NotFound'
+import UserRegister from './pages/UserRegister'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Inicio/>} />
           <Route path='/*' element={<NotFound/>} />
+          <Route path='/UserRegister' element={<UserRegister/>} />
         </Routes>
       </main>
     </BrowserRouter>
