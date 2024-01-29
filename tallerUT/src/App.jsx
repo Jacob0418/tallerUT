ded3b819c9a5e95e43c231bfdbd93f3b011f34bb
AlejandroChuc <kevin_maximilian0130@hotmail.com>
2024-01-28 19:51:40 -0500

    2.0

--- tallerUT/src/App.jsx
+++ tallerUT/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/header'
-import Inicio from './pages/Inicio'
-import NotFound from './pages/NotFound'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/header';
+import Inicio from './pages/Inicio';
+import NotFound from './pages/NotFound';
+import Actualizar from './pages/ActualizarReg';
 
 function App() {
-
   return (
     <BrowserRouter>
       <Navbar />
       <main>
         <Routes>
-          <Route path='/' element={<Inicio/>} />
-          <Route path='/*' element={<NotFound/>} />
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Actualizar" element={<ActualizarReg />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
