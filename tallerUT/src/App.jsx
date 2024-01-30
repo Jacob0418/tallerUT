7d99715bb2be073bb21d4e43dc8e2039fb7c9c12
Vlakero <egomancera@gmail.com>
2024-01-29 23:37:28 -0500

    RegistroTrabajos

--- tallerUT/src/App.jsx
+++ tallerUT/src/App.jsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/header'
 import Inicio from './pages/Inicio'
 import NotFound from './pages/NotFound'
+import RegistroTrabajo from './pages/RegistroTrabajo';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
+      
       <main>
         <Routes>
-          <Route path='/' element={<Inicio/>} />
-          <Route path='/*' element={<NotFound/>} />
+          <Route path='/' element={<Navbar/>}>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/RegistroTrabajo' element={<RegistroTrabajo />} />
+            
+            
+            <Route path='/*' element={<NotFound />} />
+          </Route>
+
         </Routes>
       </main>
     </BrowserRouter>
