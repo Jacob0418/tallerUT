4d3a064d39060388dfec844a60a06593aa557434
Jacob0418 <cjosecoronado97@gmail.com>
2024-02-03 23:44:36 -0500

    lo teniamos casi todo, ahora no tenemos nada

--- tallerUT/src/pages/Login.jsx
+++ tallerUT/src/pages/Login.jsx
@@ -24,21 +24,24 @@ function LogIn() {
 
   useEffect(() => {
     if (isAuthenticatedAdmin || isAuthenticatedMecanico) {
-      navigate("/"); // Cambia a la ruta deseada donde deben ir los usuarios después de iniciar sesión
+      navigate('/')
+    } else {
+      navigate('/login')
     }
-    // No necesitas el else aquí a menos que quieras forzar una redirección fuera de login cuando no están autenticados
-  }, [isAuthenticatedAdmin, isAuthenticatedMecanico, navigate]);
+  }, [isAuthenticatedAdmin, isAuthenticatedMecanico]);
 
   const onSubmit = async (e) => {
     try {
+      //Se espera a que las dos solicitudes esten completas
       const [responseAdmin, responseMecanico] = await Promise.all([
         loginReqAdmin(e),
         loginReqMecanico(e),
       ]);
     } catch (error) {
-      console.log("Error al realizar las solicitudes:", error);
+      console.log('Error al realizar las solicitudes')
     }
   };
+  
 
   const [show, setShow] = useState("password");
 
@@ -59,10 +62,11 @@ function LogIn() {
           <RiToolsFill className=" size-14 mb-7" />
           <div>
             {siginErrors.map((err, i) => (
-              <div className="bg-red-500 p-2 text-white" key={i}>
+              <div className="bg-red-500 p-2 text-white rounded-[7px_7px_7px_7px]" key={i}>
                 {err}
               </div>
             ))}
+            
             <div className="flex flex-col items-center">
               <label className="mb-2 font-medium">Nombre</label>
               <input
@@ -78,18 +82,12 @@ function LogIn() {
               <p className="text-red-600"> Nombre es obligatorio</p>
             )}
 
-
-
-
-
-
             <div className="flex flex-col items-center">
               <label className="font-medium mb-2">Contraseña</label>
               <span className=" relative w-full max-w-[300px]">
                 <input
                   name="password"
                   placeholder="Ingrese una contraseña"
-                  id="password"
                   type={show}
                   {...register("password", { required: true })}
                   required
@@ -101,32 +99,12 @@ function LogIn() {
 
                 <IoEyeOffOutline
                   onClick={SeePassword}
-                  id="password"
                   className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-none border-none cursor-pointer p-0"
                 >
                   {show === "password" ? <IoEyeOffOutline /> : <IoEyeOutline />}
                 </IoEyeOffOutline>
               </span>
             </div>
-
-            <div className="flex flex-col items-center">
-              <label className="mb-2 font-medium mt-5">
-                Confirmar contraseña
-              </label>
-              <input
-                name="confirmPassword"
-                placeholder="Confirma contraseña"
-                type={show}
-                id="confirmPassword"
-                {...register("confirmPassword", { required: true })}
-                required
-                className=" border-none focus:outline-red-500 focus:outline-none rounded-[5px_5px_5px_5px] p-1 w-52 mb-5"
-              />
-              {errors.confirmPassword && (
-                <p className="text-red-600"> Telefono es obligatorio</p>
-              )}
-            </div>
-
             <button className="border-2 border-red-500 bg-red-500 rounded-[5px_5px_5px_5px] p-2 font-medium mb-7 mt-7 text-white">
               Iniciar Sesión
             </button>
